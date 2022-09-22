b3bf109b624b776c772854289a865edbf6074f6e
RUiN-jiarun <1052707477@qq.com>
2022-09-22 13:41:41 +0800

    PA5 Sopus E2Rt

--- PA5/code/E2Rt.cpp
+++ PA5/code/E2Rt.cpp
@@ -3,9 +3,9 @@
 // 本程序演示如何从Essential矩阵计算R,t
 //
 
-#include <Eigen/Core>
-#include <Eigen/Dense>
-#include <Eigen/Geometry>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
+#include <eigen3/Eigen/Geometry>
 
 using namespace Eigen;
 
@@ -29,7 +29,12 @@ int main(int argc, char **argv) {
 
     // SVD and fix sigular values
     // START YOUR CODE HERE
-
+    JacobiSVD<MatrixXd> svd(E, ComputeFullU | ComputeFullV);
+    Matrix3d U = svd.matrixU();
+    Matrix3d V = svd.matrixV();
+    Vector3d sigma = svd.singularValues();
+    DiagonalMatrix<double, 3> Sigma((sigma(0) + sigma(1)) / 2, (sigma(0) + sigma(1)) / 2, 0);
+    Matrix3d Rz = AngleAxisd(M_PI/2, Vector3d(0,0,1)).toRotationMatrix();
     // END YOUR CODE HERE
 
     // set t1, t2, R1, R2 
@@ -39,6 +44,11 @@ int main(int argc, char **argv) {
 
     Matrix3d R1;
     Matrix3d R2;
+
+    t_wedge1 = U * Rz * Sigma * U.transpose();
+    R1 = U * Rz.transpose() * V.transpose();
+    t_wedge2 = U * Rz.transpose() * Sigma * U.transpose();
+    R2 = U * Rz * V.transpose();
     // END YOUR CODE HERE
 
     cout << "R1 = " << R1 << endl;
@@ -52,3 +62,18 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+// R1 =   -0.365887  -0.0584576    0.928822
+// -0.00287462    0.998092   0.0616848
+//    0.930655  -0.0198996    0.365356
+// R2 =  -0.998596  0.0516992 -0.0115267
+// -0.0513961   -0.99836 -0.0252005
+//  0.0128107  0.0245727  -0.999616
+// t1 =  -0.581301
+// -0.0231206
+//   0.401938
+// t2 =  0.581301
+// 0.0231206
+// -0.401938
+// t^R =  -0.0203619   -0.400711  -0.0332407
+//    0.393927   -0.035064    0.585711
+// -0.00678849   -0.581543  -0.0143826
